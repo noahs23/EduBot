cdc1be43b0a9df313171132ef48e5c0cc723c61f
Noah <noahburtonsmith@gmail.com>
2025-12-16 13:51:02 -0600

    mvp commit

--- src/main.py
+++ src/main.py
@@ -13,30 +13,62 @@
 
 # Library imports
 from vex import *
-import testmath
+
 # Brain should be defined by default
 brain=Brain()
 
 # Robot configuration code
-left1_motor = Motor(Ports.PORT11)
-left2_motor = Motor(Ports.PORT12)
+left1_motor = Motor(Ports.PORT11, False)
+left2_motor = Motor(Ports.PORT12, False)
 left_drive_group = MotorGroup(left1_motor, left2_motor)
-right1_motor = Motor(Ports.PORT8)
-right2_motor = Motor(Ports.PORT10)
+right1_motor = Motor(Ports.PORT8, True)
+right2_motor = Motor(Ports.PORT10, True)
 right_drive_group = MotorGroup(right1_motor, right2_motor)
 
 drivetrain = DriveTrain(left_drive_group, right_drive_group, 319.19, 295, 40, MM, 1)
 
 # Begin project code
 
+import math
+
+def degrees_per_second_to_m_s(degrees_per_second, radius_meters):
+    """
+    Converts angular velocity in degrees per second to linear velocity in meters per second.
+
+    Args:
+        degrees_per_second (float): The angular velocity to convert.
+        radius_meters (float): The radius of the rotation in meters.
+
+    Returns:
+        float: The linear velocity in meters per second.
+    """
+    # 1. Convert degrees per second to radians per second
+    # There are 180 degrees in pi radians
+    radians_per_second = degrees_per_second * (math.pi / 180.0)
+    
+    # 2. Calculate linear velocity (v = omega * r)
+    # v (m/s) = omega (rad/s) * r (m)
+    linear_velocity_m_s = radians_per_second * radius_meters
+    
+    return linear_velocity_m_s
+
+# --- Example Usage ---
+# Example: A point on a wheel rotating at 90 degrees/sec, 
+# 0.5 meters from the center (radius).
+velocityMS = 0.19
+drivetrain.drive(FORWARD)
+wait(1/velocityMS, SECONDS)
+drivetrain.stop()
+
 
 # Print all Drivetrain sensing values to the screen in an infinite loop
 while True:
     # Clear the screen and set the cursor to top left corner on each loop
+    velocity = degrees_per_second_to_m_s(drivetrain.velocity(VelocityUnits.DPS), 0.034925)
     brain.screen.clear_screen()
     brain.screen.set_cursor(1,1)
 
-    brain.screen.print("Velocity:", drivetrain.velocity(PERCENT))
+    brain.screen.print("Velocity:", velocity)
     brain.screen.next_row()
 
     brain.screen.print("Current:", drivetrain.current(CurrentUnits.AMP))
@@ -57,11 +89,6 @@ while True:
     # A brief delay to allow text to be printed without distortion or tearing
     wait(100,MSEC)
     
-    
-    
-    drivetrain.drive(FORWARD)
-    velocity = testmath.degrees_per_second_to_m_s(drivetrain.velocity(VelocityUnits.DPS), 0.034925)
-    print(velocity)
-    wait(5000, MSEC)
-    drivetrain.stop
+
+
     
