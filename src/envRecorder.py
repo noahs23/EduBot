b7996e1bbff97357f437bbf63000174a37c5826f
Noah <noahburtonsmith@gmail.com>
2026-01-23 15:29:04 -0600

    added data_buffer.csv

--- src/envRecorder.py
+++ src/envRecorder.py
@@ -9,11 +9,13 @@ def recorder(numOfDataEntries, polling_delay_msec):
         data_buffer += "%1.3f" % brain.timer.value() + ", "
         data_buffer += "%1.3f" % brain_inertial.acceleration(XAXIS) + ", " + "\n"
         brain.screen.print(data_buffer)
+        # with open("data_buffer.csv", "a") as f:
+        #     f.write(data_buffer)
         wait(polling_delay_msec)
         brain.screen.new_line()
         if (brain.screen.row() == 12):
             brain.screen.set_cursor(1, 1)
             brain.screen.clear_screen()
-            
+        
 
-recorder(100, 500)
+recorder(20, 500)
